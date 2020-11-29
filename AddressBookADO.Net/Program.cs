efd84ebaaf88feba0acb53e76e3d86c7ceb9a405
Ramya.U <ramyayadav2016@gmail.com>
2020-11-29 14:39:19 +0530

    [Ramya] Add . Addded method to add new contacts

--- AddressBookADO.Net/Program.cs
+++ AddressBookADO.Net/Program.cs
@@ -15,6 +15,29 @@ namespace AddressBookADO.Net
             AddressBookRepository repository = new AddressBookRepository();
             ///UC1 Creating a method for checking for the validity of the connection.
             repository.EnsureDataBaseConnection();
+            /// UC2 Getting all the stored records in the address book service table by fetching all the records
+            repository.GetAllContact();
+            /// UC 3: Adds the new contact into DB table.
+            AddNewContactDetails();
+        }
+        /// UC 3: Adds the new contact into DB table.
+        public static void AddNewContactDetails()
+        {
+            AddressBookRepository repository = new AddressBookRepository();
+            AddressBookModel model = new AddressBookModel();
+            model.FirstName = "Daksha";
+            model.LastName = "Neel";
+            model.Address = "Sadashivanagar";
+            model.City = "Bnagalore";
+            model.State = "Karnataka";
+            model.Zip = 788776;
+            model.PhoneNumber = 887788779;
+            model.EmailId = "daksh@gmail.com";
+            model.AddressBookName = "neha";
+            model.AddressBookType = "Friend";
+            Console.WriteLine(repository.AddDataToTable(model) ? "Record inserted successfully\n" : "Failed");
         }
     }
 }
+
+  
