19b2035960294362dc06699fe549316018c821ce
Ramya.U <ramyayadav2016@gmail.com>
2020-11-29 14:26:18 +0530

    Add project files.

---
+++ AddressBookADO.Net/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AddressBookADO.Net
+{
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to AddressBookADO.Net!");
+            ///Creating Instance object of AddressBookRepository class.
+            AddressBookRepository repository = new AddressBookRepository();
+            ///UC1 Creating a method for checking for the validity of the connection.
+            repository.EnsureDataBaseConnection();
+        }
+    }
+}
